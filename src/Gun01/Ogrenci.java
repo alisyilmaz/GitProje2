02332e034e3f53a9af1317499d0a1133eaa62405
alisyilmaz <alis_yilmaz@hotmail.com>
2023-01-26 20:09:11 +0300

    task 3yapıldı

--- src/Gun01/Ogrenci.java
+++ src/Gun01/Ogrenci.java
@@ -1,4 +1,9 @@
 package Gun01;
 
 public class Ogrenci {
+    public static void main(String[] args) {
+        System.out.println("dnemee");
+        System.out.println("task 2 yapıldı");
+        System.out.println("merhaba 3");
+    }
 }
